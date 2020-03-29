db1f794cc2182f98ec73bd105c2b1c6ce3102160
rakgwonjung <jrg8989@gmail.com>
2020-03-29 12:03:07 +0900

    gradle(lombok, jpa) add \ lombok test code add

---
+++ src/test/java/com/jrock/boot/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.jrock.boot.springboot.dto;
+
+import com.jrock.boot.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
